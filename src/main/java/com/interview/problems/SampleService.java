b89f4a6feb8cf72e16657d501d737c47d4974667
machin3head <thomas8cyber@gmail.com>
2022-08-08 10:11:58 -0500

    refactor naming, add new method

---
+++ src/main/java/com/interview/problems/SampleService.java
@@ -0,0 +1,52 @@
+package com.interview.problems;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SampleService {
+
+    // input List will contain 1 or more elements and less than 100 elements
+    // TODO: return the earliest chronological date from the given list
+    public LocalDateTime getEarliestDateFromList(List<LocalDateTime> datesList) {
+        return datesList.get(0);
+    }
+
+
+    // input array will contain 1 or more elements and less than 100 elements
+    // TODO: return the sum of all elements in the given array
+    //  sample: array [3,2,80]
+    //  return: 85
+    public int sumAllElements(int[] arr) {
+        return 1;
+    }
+
+
+    // input array will contain 1 or more elements and less than 100
+    // TODO: return the max consecutive count that the given int occurs in array
+    //  sample: nums[0,0,1,1,1,0,1,0,0,1], comp=1
+    //  return: 3
+    public int findMaxConsecutiveOccurrences(int[] nums, int given) {
+        return nums[0];
+    }
+
+    // map will contain 1 or more entries and less than 32 entries
+    // map keys will be pre-sorted (ascending) smallest to largest (1...31)
+    // sample map:
+    //    {1=90} day 1 was 90 degrees
+    //    {2=87} day 2 had 87 degrees
+    //    {3=90} day 3 had 90 degrees
+    // TODO: return the earliest day (smallest key) if multiple days have the same highest temperature count
+    //  example: days 1 and 3 both have the highest temperature of 90 degrees, 1 should be returned
+    public int getDayOfMonthWithHighestTemperature(LinkedHashMap<Integer, Integer> dayWithTemperatureMap) {
+        int highestTemperature = 0;
+        int dayOfMonthWithHighestTemp = 1;
+        for (Map.Entry<Integer, Integer> entry : dayWithTemperatureMap.entrySet()) {
+            if (entry.getValue() > highestTemperature) {
+                dayOfMonthWithHighestTemp = entry.getKey();
+            }
+        }
+        return dayOfMonthWithHighestTemp;
+    }
+}
--- src/test/java/com/interview/problems/SampleServiceTest.java
+++ src/test/java/com/interview/problems/SampleServiceTest.java
@@ -1,4 +1,4 @@
-package com.interview.standalone;
+package com.interview.problems;
 
 import org.junit.jupiter.api.Test;
 
@@ -6,16 +6,17 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DateServiceTest {
+public class SampleServiceTest {
 
-    private final DateService testClass = new DateService();
+    private final SampleService testClass = new SampleService();
 
     @Test
-    public void getEarliestDate_HappyPath() {
+    public void getEarliestDateFromList() {
         LocalDateTime _2021_04_10_endOfDay = LocalDateTime.of(2021, Month.APRIL, 10, 23, 59, 59, 999999999);
         LocalDateTime _2021_05_11_endOfDay = LocalDateTime.of(2021, Month.MAY, 11, 23, 59, 59, 999999999);
         LocalDateTime _2022_03_10_endOfDay = LocalDateTime.of(2022, Month.MARCH, 10, 23, 59, 59, 999999999);
@@ -32,29 +33,37 @@ public class DateServiceTest {
         assertEquals(_2021_04_10_endOfDay, result);
     }
 
-
     @Test
-    public void getTotalTransactionsForMonth() {
+    public void sumAllElements() {
         int[] transactions = {132,54,65,8,97,1,34,6,44,47,87,346,76,123,43};
 
-        int result = testClass.getSumOfAllTransactions(transactions);
+        int result = testClass.sumAllElements(transactions);
 
         int expectedTotal = 1163;
         assertEquals(expectedTotal, result);
     }
 
+    @Test
+    public void findMaxConsecutiveOnes() {
+        int[] nums = {0,0,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1};
+        int var = 1;
+
+        int result = testClass.findMaxConsecutiveOccurrences(nums, var);
+
+        int expectedTotal = 5;
+        assertEquals(expectedTotal, result);
+    }
 
     @Test
     public void getFirstDayWithHighestTransactions() {
-        HashMap<Integer, Integer> dayToCountMap = new HashMap<>(31);
+        LinkedHashMap<Integer, Integer> dayToCountMap = new LinkedHashMap<>(31);
         loadMap(dayToCountMap);
 
-        int result = testClass.getDayOfMonthWithMostTransactions(dayToCountMap);
+        int result = testClass.getDayOfMonthWithHighestTemperature(dayToCountMap);
 
         int dayWithHighestValue = getExpectedValue();
         assertEquals(dayWithHighestValue, result);
     }
-
 
     private void loadMap(HashMap<Integer, Integer> dayToCountMap) {
         for (int i=1; i<15; i++) {
