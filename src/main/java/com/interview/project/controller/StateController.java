96449f9c03b39970c800fa40b7c9f13c5ec01c97
machin3head <thomas8cyber@gmail.com>
2022-09-01 15:39:16 -0500

    add spring web starter dep, add classes to project package

---
+++ src/main/java/com/interview/project/controller/StateController.java
@@ -0,0 +1,12 @@
+package com.interview.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StateController {
+
+    // TODO create a GET API to return all states
+
+    // TODO create a GET API to return the total count of states that start with A
+
+}
---
+++ src/main/java/com/interview/project/service/StateService.java
@@ -0,0 +1,8 @@
+package com.interview.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StateService {
+
+}
